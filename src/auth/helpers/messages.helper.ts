c6a9d7f635cd47395da9ed23b687e03fab9fc8f5
sdanielsouza2023 <sdanielsouza2023@gmail.com>
2023-10-04 01:30:44 -0300

    Criando controle de login, pasta auth com arquivos dtos e helpers

---
+++ src/auth/helpers/messages.helper.ts
@@ -0,0 +1,5 @@
+export const MessagesHelper = {
+        AUTH_LOGIN_NOT_FOUND: "Campo email não é Inválido.",
+        AUTH_PASSWORD_NOT_FOUND: "Campo senha não é Inválido..",
+        AUTH_PASSWORD_OR_LOGIN_NOT_FOUND: "Campo Usuario ou Senha Inválido."
+}
