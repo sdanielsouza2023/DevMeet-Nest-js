bd563cc8cf01ecca5165b32c98c34956b039de98
sdanielsouza2023 <sdanielsouza2023@gmail.com>
2023-10-05 21:21:22 -0300

    Aula 02 de 02, criando API de login

--- src/auth/dtos/login.dto.ts
+++ src/auth/dtos/login.dto.ts
@@ -1,7 +1,7 @@
 import { IsEmail, IsNotEmpty } from "class-validator"
 import { MessagesHelper } from "../helpers/messages.helper"
 
-export class loginDto {
+export class LoginDto {
     @IsEmail({},{message:MessagesHelper.AUTH_LOGIN_NOT_FOUND})
     login:string
     @IsNotEmpty({message:MessagesHelper.AUTH_PASSWORD_NOT_FOUND})
